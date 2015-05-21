f2ba9a6d000fc5ef412b06e500e8e9ada8d396bc
Michael Crilly <mrcrilly@gmail.com>
2015-05-21 22:58:18 +1000

    Using absolute imports to avoid confusion and potential breakage.

--- journal/__init__.py
+++ journal/__init__.py
@@ -6,13 +6,14 @@ def create_app(config_file):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
 
-    from database import init_logstore, init_documentstore
+    # from database import init_logstore, init_documentstore
+    from journal.database import init_logstore, init_documentstore
 
     app.config['db_conn'] = init_documentstore(app)
     app.config['logging'] = init_logstore(app)
 
-    import application
-    import filters
+    import journal.application
+    import journal.filters
 
     app.register_blueprint(filters.blueprint)
     app.register_blueprint(application.blueprint)
